ae378addb1943d27b050986878b9090be5b80270
Spence Peacock <spencerpeacock@gmail.com>
2017-05-08 17:00:15 -0500

    string and list practice

---
+++ spence_peacock/SALP.py
@@ -0,0 +1,11 @@
+str = "it's thanksgiving day it's my birthday too!"
+print str.find('day')
+x=[2,54,-2,7,12,98]
+print min(x)
+print max(x)
+print x[0]
+print x[len(x)-1]
+"hello wisconsin".split(" ")
+['hello', 'wisconsin']
+
+
