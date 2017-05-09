25d65ac67eb21531b94cb3c5448e7faba35f4d6c
Spence Peacock <spencerpeacock@gmail.com>
2017-05-09 16:34:54 -0500

    scores and grades excercise

---
+++ spence_peacock/scoresAndGrades.py
@@ -0,0 +1,16 @@
+import random
+grade = randint(60,100)
+def grademe(grade):
+    if grade>=60 and grade<70:
+         print "your score is: {} So your grade is a D".format(grade)
+    elif grade>=70 and grade<80:
+         print "your score is: {} So your grade is a C".format(grade)
+    elif grade>=80 and grade<90:
+         print "your score is: {} your grade is a B".format(grade)
+    elif grade>=90 and grade<101:
+         print "your score is: {} your grade is an A".format(grade)
+
+        
+
+        
+    
