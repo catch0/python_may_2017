d2291ce9dfb9dfb12fc4200e853c6a1dc5babbc6
Spence Peacock <spencerpeacock@gmail.com>
2017-05-09 23:29:20 -0500

    dictionary excercise

---
+++ spence_peacock/dictionaries.py
@@ -0,0 +1,10 @@
+spence={
+    "first_name": "spence",
+    "age" : 32,
+    "birthplace" : "provo",
+    "language" : "python",
+}
+print "my name is " + spence["first_name"]
+print "my age is {}".format(spence["age"])
+print "my city of birth is " + spence["birthplace"]
+print "my favourite language is " + spence["language"]
